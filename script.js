c5964cb9068b4d799133eb497a8d3e3d00afc351
Grimmjow86 <85533763+Grimmjow86@users.noreply.github.com>
2021-11-13 18:37:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll(".panel");
+
+for (let i = 0; i < panels.length; i++) {
+	panels[i].addEventListener("click", () => {
+		for (let j = 0; j < panels.length; j++) {
+			panels[j].classList.remove("active");
+		}
+		panels[i].classList.add("active");
+	});
+}
